16cdf73cc8ab8b07d847bdcc80ecd08bf5913c41
kapehh <git@kapehh.net>
2015-02-19 19:02:48 +0400

    gaga

--- src/me/kapehh/main/RandomChest/Config.java
+++ src/me/kapehh/main/RandomChest/Config.java
@@ -2,7 +2,11 @@ package me.kapehh.main.RandomChest;
 
 import me.kapehh.main.pluginmanager.config.EventPluginConfig;
 import me.kapehh.main.pluginmanager.config.EventType;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by Karen on 19.02.2015.
@@ -11,6 +15,18 @@ public class Config {
 
     @EventPluginConfig(EventType.LOAD)
     public void onLoadConfig(FileConfiguration cfg) {
-        System.out.println("LOADING CFG: " + cfg.getBoolean("debug"));
+        ConfigurationSection chestSection = cfg.getConfigurationSection("Chests");
+        Set<String> chests = chestSection.getKeys(false);
+
+        for (String chest : chests) {
+            ConfigurationSection inChest = chestSection.getConfigurationSection(chest);
+            Set<String> items = inChest.getKeys(false);
+
+            for (String item : items) {
+                ConfigurationSection inItem = inChest.getConfigurationSection(item);
+
+                System.out.println("ITEM " + item + " id " + inItem.getInt("Id"));
+            }
+        }
     }
 }
